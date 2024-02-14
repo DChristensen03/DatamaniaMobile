73320523ded29eb22d446a1aea512479c7f4c99d
wiathe01 <wiathe01@wsc.edu>
2024-02-14 16:54:47 -0600

    added tutorial page placeholder text and video

--- datamania/components/tutorial.component.js
+++ datamania/components/tutorial.component.js
@@ -1,14 +1,39 @@
 import React from "react";
 import { Button, Text, Icon, Layout } from "@ui-kitten/components";
 import { SafeAreaView, StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default (props) => {
 	return (
 		<SafeAreaView style={{ flex: 1 }}>
-			<Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-				<Text style={styles.text} category="h1">
-					Tutorial
+			<Layout style={{ flex: 1, justifyContent: "flex-start", alignItems: "center" }}>
+				<Text style={styles.text1} category="h1">
+					How it Works
 				</Text>
+			</Layout>
+			<Layout style={{ flex: 2, justifyContent: "flex-start", alignItems: "left" }}>
+				<Text style={styles.text} category="h6">
+					* There will be questions
+				</Text>
+				<Text style={styles.text} category="h6">
+					* You will try to answer them
+				</Text>
+				<Text style={styles.text} category="h6">
+					* You'll get a score at some point
+				</Text>
+				<Text style={styles.text} category="h6">
+					* (Placeholder)
+				</Text>
+			</Layout>
+			<Layout style={{ flex: 1, justifyContent: "flex-start", alignItems: "center" }}>
+				<Text style={styles.text} category="h1">
+					How-to Video
+				</Text>
+			</Layout>
+			<Layout style={{ flex: 1, justifyContent: "flex-start", alignItems: "center" }}>
+				<Layout style={styles.videoContainer}>
+					<WebView source={{ uri: 'https://www.youtube.com/watch?v=CVsbTCdTyAM&ab_channel=TheCrazyLoopGuy' }} />
+				</Layout>
 			</Layout>
 		</SafeAreaView>
 	);
@@ -21,9 +46,23 @@ const styles = StyleSheet.create({
 		width: 175,
 		height: 50,
 	},
-	text: {
+	text1: {
+		//alignItems: "",
 		fontWeight: "bold",
 		fontStyle: "italic",
+		paddingTop: 50,
+	},
+	text: {
+		fontWeight: "bold",
+		//fontStyle: "italic",
 		paddingBottom: 50,
 	},
+	videoContainer: {
+		width: '100%',
+		aspectRatio: 16 / 9, // You can adjust the aspect ratio as needed
+		marginBottom: 20,
+	},
+	video: {
+		flex: 1,
+	}
 });
