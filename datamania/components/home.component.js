1adf0e61f99619dddec2e236f994d58f522ae080
DChristensen03 <dylanchristensen2000@gmail.com>
2024-04-22 19:55:09 -0500

    Fixes

--- datamania/components/home.component.js
+++ datamania/components/home.component.js
@@ -3,75 +3,64 @@ import { Button, Text, Icon, Layout } from "@ui-kitten/components";
 import { SafeAreaView, StyleSheet, View } from "react-native";
 
 export default ({ navigation }) => {
-  const navigatePlay = () => {
-    navigation.navigate("Play");
-  };
+	const navigatePlay = () => {
+		navigation.navigate("Play");
+	};
 
-  const navigateTutorial = () => {
-    navigation.navigate("Tutorial");
-  };
+	const navigateTutorial = () => {
+		navigation.navigate("Tutorial");
+	};
 
-  const navigateResults = () => {
-    navigation.navigate("Results");
-  };
-
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Layout
-        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-      >
-        <View
-          style={{ flex: 2, justifyContent: "flex-end", alignItems: "center" }}
-        >
-          <Text style={styles.text} category="h1">
-            DataMania
-          </Text>
-        </View>
-        <View
-          style={{
-            flex: 3,
-            justifyContent: "flex-start",
-            alignItems: "center",
-          }}
-        >
-          <Button
-            style={styles.button}
-            accessoryRight={PlayIcon}
-            size="giant"
-            onPress={navigatePlay}
-          >
-            Play
-          </Button>
-          <Button
-            style={styles.button}
-            accessoryRight={BulbIcon}
-            size="giant"
-            onPress={navigateTutorial}
-          >
-            Learn
-          </Button>
-          <Button style={styles.button} size="giant" onPress={navigateResults}>
-            Results
-          </Button>
-        </View>
-      </Layout>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView style={{ flex: 1 }}>
+			<Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+				<View style={{ flex: 2, justifyContent: "flex-end", alignItems: "center" }}>
+					<Text style={styles.text} category="h1">
+						DataMania
+					</Text>
+				</View>
+				<View
+					style={{
+						flex: 3,
+						justifyContent: "flex-start",
+						alignItems: "center",
+					}}
+				>
+					<Button
+						style={styles.button}
+						accessoryRight={PlayIcon}
+						size="giant"
+						onPress={navigatePlay}
+					>
+						Play
+					</Button>
+					<Button
+						style={styles.button}
+						accessoryRight={BulbIcon}
+						size="giant"
+						onPress={navigateTutorial}
+					>
+						Learn
+					</Button>
+				</View>
+			</Layout>
+		</SafeAreaView>
+	);
 };
 
 const PlayIcon = (props) => <Icon name="play-circle-outline" {...props} />;
 const BulbIcon = (props) => <Icon name="bulb-outline" {...props} />;
 
 const styles = StyleSheet.create({
-  button: {
-    fontWeight: "bold",
-    margin: 10,
-    width: 175,
-    height: 50,
-  },
-  text: {
-    fontWeight: "bold",
-    fontStyle: "italic",
-    paddingBottom: 50,
-  },
+	button: {
+		fontWeight: "bold",
+		margin: 10,
+		width: 175,
+		height: 50,
+	},
+	text: {
+		fontWeight: "bold",
+		fontStyle: "italic",
+		paddingBottom: 50,
+	},
 });
--- datamania/data/game_5.js
+++ datamania/data/game_5.js
@@ -2,51 +2,175 @@ export default questions = [
 	{
 		title: "D",
 		questions: [
-			{ points: 100, question: "?", answer: "Digital" },
-			{ points: 200, question: "?", answer: "Dell" },
-			{ points: 300, question: "?", answer: "Discord" },
-			{ points: 400, question: "?", answer: "Direct Current (DC)" },
-			{ points: 500, question: "?", answer: "Delimeter" },
+			{
+				points: 100,
+				question:
+					"What term which originally referred to non-analog signals now often refers to anything virtual or internet-based?",
+				answer: "Digital",
+			},
+			{
+				points: 200,
+				question:
+					"What computer company sells products such as the Inspiron, Latitude, and OptiPlex?",
+				answer: "Dell",
+			},
+			{
+				points: 300,
+				question:
+					"What social platform, primarily associated with gaming, provides messaging, voice chat, and video call functionality?",
+				answer: "Discord",
+			},
+			{
+				points: 400,
+				question:
+					"Which type of electrical current provides mostly constant one-directional electric charge?",
+				answer: "Direct Current (DC)",
+			},
+			{
+				points: 500,
+				question:
+					"What term, often associated with CSV files, refers to characters that specify the boundary between different pieces of data?",
+				answer: "Delimeter",
+			},
 		],
 	},
 	{
 		title: "E",
 		questions: [
-			{ points: 100, question: "?", answer: "Excel" },
-			{ points: 200, question: "?", answer: "" },
-			{ points: 300, question: "?", answer: "" },
-			{ points: 400, question: "?", answer: "" },
-			{ points: 500, question: "?", answer: "" },
+			{
+				points: 100,
+				question:
+					"What is the name of the major spreadsheet software developed by Microsoft?",
+				answer: "Excel",
+			},
+			{
+				points: 200,
+				question:
+					"Electricity is caused by the flow of many of what type of sub-atomic particle?",
+				answer: "Electron",
+			},
+			{
+				points: 300,
+				question:
+					"What is the process of using algorithms to hide data so that it can be securely transferred?",
+				answer: "Encryption",
+			},
+			{
+				points: 400,
+				question:
+					"What kind of technology is used to connect Local-Area Networks via cables?",
+				answer: "Ethernet",
+			},
+			{
+				points: 500,
+				question:
+					"What datatype in C++ and related languages uses values that represent a finite set of constants?",
+				answer: "Enum (enumeration)",
+			},
 		],
 	},
 	{
 		title: "B",
 		questions: [
-			{ points: 100, question: "?", answer: "" },
-			{ points: 200, question: "?", answer: "" },
-			{ points: 300, question: "?", answer: "" },
-			{ points: 400, question: "?", answer: "" },
-			{ points: 500, question: "?", answer: "" },
+			{
+				points: 100,
+				question:
+					"What wireless technology uses radio waves to transfer data to paired devices within 33ft of the device?",
+				answer: "Bluetooth",
+			},
+			{
+				points: 200,
+				question:
+					"What is the term for forms of identification such as fingerprinting that are based on distinct physiological characteristics?",
+				answer: "Biometrics",
+			},
+			{
+				points: 300,
+				question:
+					"What term refers to either preinstalled unwanted software that takes up space or software that slows down a machine with ineficciency?",
+				answer: "Bloatware",
+			},
+			{
+				points: 400,
+				question:
+					"Usually measured in hertz, what term refers to the maximum rate of data transfer in a network?",
+				answer: "Bandwidth",
+			},
+			{
+				points: 500,
+				question:
+					'Sometimes called a "data highway", what term refers to pathways of data transfer, typically connected to the CPU?',
+				answer: "Bus",
+			},
 		],
 	},
 	{
 		title: "U",
 		questions: [
-			{ points: 100, question: "?", answer: "" },
-			{ points: 200, question: "?", answer: "" },
-			{ points: 300, question: "?", answer: "" },
-			{ points: 400, question: "?", answer: "" },
-			{ points: 500, question: "?", answer: "" },
+			{
+				points: 100,
+				question:
+					"What is the process of transfering data from one system to a larger system, usually the internet?",
+				answer: "Uploading",
+			},
+			{
+				points: 200,
+				question: "What is the technical term for a website's address?",
+				answer: "Uniform Resource Locator (URL)",
+			},
+			{
+				points: 300,
+				question:
+					"What game engine associated with indie games creates 2D and 3D games and competes with the Unreal Engine?",
+				answer: "Unity",
+			},
+			{
+				points: 400,
+				question:
+					"What common standard for text representation can represent more symbols than ASCII?",
+				answer: "Unicode",
+			},
+			{
+				points: 500,
+				question:
+					"What device is used to provide constant battery power to a device in the event of a disruption?",
+				answer: "Uninterruptable Power Supply (UPS)",
+			},
 		],
 	},
 	{
 		title: "G",
 		questions: [
-			{ points: 100, question: "?", answer: "" },
-			{ points: 200, question: "?", answer: "Ground" },
-			{ points: 300, question: "?", answer: "GitHub" },
-			{ points: 400, question: "?", answer: "" },
-			{ points: 500, question: "?", answer: "---" },
+			{
+				points: 100,
+				question:
+					"What technology company started as a major search engine and now makes phones, a web browser, and applications of all kinds?",
+				answer: "Google",
+			},
+			{
+				points: 200,
+				question:
+					"What is the unit for a frequency of a billion cycles per second?",
+				answer: "Gigahertz",
+			},
+			{
+				points: 300,
+				question:
+					"In electronics, what term refers to either a voltage reference point or the place that electricity drains into?",
+				answer: "Ground",
+			},
+			{
+				points: 400,
+				question:
+					"What platform is used by software developers to collaborate and manage versions of code?",
+				answer: "GitHub",
+			},
+			{
+				points: 500,
+				question:
+					"What four-letter acronym refers to the concept that the quality of an operation's results depend on the quality of its inputs?",
+				answer: "GIGO (Garbage In, Garbage Out)",
+			},
 		],
 	},
 ];
